375a7786d46b9dca7fed47a29b473689f83f1e8a
EC2 Default User <ec2-user@ip-172-31-35-234.ap-northeast-1.compute.internal>
2025-06-08 08:52:17 +0000

    [finish]add Admin/Public

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
-  root to: "homes#top"
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+  
+  scope module: :public do
+    devise_for :users
+    root to: "homes#top"
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resource :favorite, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
+  end
 
   get '/homes/about' => 'homes#about', as:'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
